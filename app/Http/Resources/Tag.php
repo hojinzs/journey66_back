779d2e15229cd8968409fa8e846ed9b86cb467b3
Steve Lee <hojinzs@gmail.com>
2020-05-16 00:59:10 +0900

    # infraPlace 코드 정리 - apiResourceManager.js 적용 - tagList api 연결 추가 작업

--- app/Http/Resources/Tag.php
+++ app/Http/Resources/Tag.php
@@ -17,12 +17,14 @@ class Tag extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
+            'description' => $this->description,
             'type' => $this->tagType->label,
             'tag_Type' => $this->tagType,
             'label' => $this->label,
             'icon_prefix' => $this->icon_prefix,
             'icon_name' => $this->icon_name,
             'color' => $this->color,
+            'pivot' => $this->pivot,
         ];
     }
 }
